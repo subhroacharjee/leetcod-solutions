c277e6b50938c69180086716f21967a2380ba9b2
Subhro Acharjee <88422396+subhroacharjee@users.noreply.github.com>
2022-11-24 09:24:51 +0530

    Time: 179 ms (58.28%), Space: 42.5 MB (32.21%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,52 @@
+class Solution{
+    
+   char[][] matrix;
+    String word;
+    int n, m;
+    public boolean exist(char[][] board, String word) {
+        n = board.length;
+        m = board[0].length;
+        matrix = board;
+        this.word = word;
+        HashSet<Character> charSet = new HashSet<>();
+        for (int i=0; i < word.length(); i++) {
+            charSet.add(word.charAt(i));
+        }
+        
+      
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (!charSet.contains(board[i][j])) matrix[i][j] = '#';
+            }
+        }
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+               if (matrix[i][j]!='#' && func(i, j, 0)) return true; 
+            }
+        }
+        
+        return false;
+    }
+    
+    
+    
+    boolean func(int i, int j, int wordIndex) {
+        if(wordIndex == word.length()) return true;
+        if ( i < 0 || i >= n || j < 0 || j >= m || matrix[i][j] != word.charAt(wordIndex)) return false;
+        
+
+        matrix[i][j] = '#';
+        boolean ans1 = func(i+1, j, wordIndex +1) 
+                        || func(i-1, j, wordIndex+1) 
+                        || func(i, j+1, wordIndex+1) 
+                        || func(i, j-1, wordIndex+1);
+        matrix[i][j] = word.charAt(wordIndex);
+        
+        return ans1;
+        
+    }
+    
+}
+
