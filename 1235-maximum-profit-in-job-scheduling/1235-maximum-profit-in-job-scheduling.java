7fcfd0a30f6e7af9717d620d02b96b0748a355bd
Subhro Acharjee <88422396+subhroacharjee@users.noreply.github.com>
2022-11-26 11:50:52 +0530

    Time: 36 ms (89.50%), Space: 76.1 MB (29.01%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n = startTime.length;
+        int[][] jobs = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            jobs[i][0] = startTime[i];
+            jobs[i][1] = endTime[i];
+            jobs[i][2] = profit[i];
+        }
+
+        Arrays.sort(jobs, (a, b) -> a[1] - b[1]);
+
+        int[] dp = new int[n];
+        dp[0] = jobs[0][2];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(jobs[i][2], dp[i-1]);
+            for (int j = i - 1; j >= 0; j--) {
+                if (jobs[j][1] <= jobs[i][0]) {
+                    dp[i] = Math.max(dp[i], jobs[i][2] + dp[j]);
+                    break; // 不加这个会超时
+                }
+            }
+        }
+
+        int ans = Integer.MIN_VALUE;
+        for (int val: dp) {
+            ans = Math.max(ans, val);
+        }
+        return ans;
+    }
+}
