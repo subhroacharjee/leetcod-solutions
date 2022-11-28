7b072e23acafcb401b0d3063008381acd70afedc
Subhro Acharjee <88422396+subhroacharjee@users.noreply.github.com>
2022-11-28 08:19:12 +0530

    Time: 104 ms (87.75%), Space: 157.9 MB (33.26%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,29 +1,32 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int MAX_LENGTH = 100001;
+        int[] players = new int[MAX_LENGTH];
+        for(int i = 0; i < MAX_LENGTH; i++) players[i] = -1;
         
-        for (int i = 0; i < matches.length; i ++) {
+        int max = 0;
+        int min = MAX_LENGTH;
+        
+        for(int i = 0; i < matches.length; i++) {
             int winner = matches[i][0];
             int loser = matches[i][1];
             
-            if (!map.containsKey(loser)) {
-                map.put(loser,1);
-            } else {
-                map.put(loser, map.get(loser)+1);
-            }
+            max = Integer.max(max, Integer.max(winner, loser));
+            min = Integer.min(min, Integer.min(winner, loser));
+            if (players[loser] == -1) players[loser] = 1;
+            else players[loser]++;
             
-            if (!map.containsKey(winner)) {
-                map.put(winner, 0);
-            }
+            if (players[winner] == -1) players[winner] = 0;
         }
         
         List<List<Integer>> ans = new ArrayList<>();
         
         ans.add(new ArrayList<>());
         ans.add(new ArrayList<>());
-        for (int player : map.keySet()) {
-            if (map.get(player) == 0) ans.get(0).add(player);
-            else if (map.get(player) == 1) ans.get(1).add(player);
+        for (int player = min; player <max+1; player++) {
+            if (players[player] == -1) continue;
+            else if (players[player] == 0) ans.get(0).add(player);
+            else if (players[player] == 1) ans.get(1).add(player);
         }
         
         return ans;
