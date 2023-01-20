01265eed14fa86ed59b625ec197f58f83eb3112c
Subhro Acharjee <88422396+subhroacharjee@users.noreply.github.com>
2023-01-21 00:40:00 +0530

    Time: 13 ms (91.98%), Space: 49.8 MB (87.19%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -1,47 +1,49 @@
 class Solution {
     public boolean possibleBipartition(int n, int[][] dislikes) {
+        int[] colors = new int[n+1];
         List<Integer>[] adjList = new List[n+1];
-        int[] groups = new int[n+1];
         
-        for (int i = 1; i <  n+1; i++) adjList[i] = new ArrayList<>();
+        // create graph
+        for (int i = 0; i <= n; i++) adjList[i] = new ArrayList<>();
         
-        for (int i = 0; i < dislikes.length; i++) {
-            adjList[dislikes[i][0]].add(dislikes[i][1]);
-            adjList[dislikes[i][1]].add(dislikes[i][0]);
+        for (int[] dislike: dislikes) {
+            int u = dislike[0], v = dislike[1];
+            adjList[u].add(v);
+            adjList[v].add(u);
         }
         
-    
-        for( int i = 1; i < n+1; i++) {
-            if (groups[i] == 0) {
-                if (!dfs(i, adjList, groups, 1)) return false;
+        // color the graph
+        Queue<Integer> q = new LinkedList<>();
+        
+        
+        for (int i = 1; i <= n; i++) {
+            if (colors[i]!=0) continue;
+            q.add(i);
+            colors[i] = 1;
+            while(!q.isEmpty()) {
+                int node = q.poll();
+                int nextColor = colors[node] == 1? -1: 1;
+                for (int adjNode: adjList[node]) {
+                    if (colors[adjNode] != 0) {
+                        if (colors[adjNode] != nextColor) return false;
+                        else continue;
+                    }
+                    colors[adjNode] = nextColor;
+                    q.add(adjNode);
+                }
+            }
+        
+        }
+        
+        // validating if grouping is possible or not
+        for (int i = 1; i <= n; i++) {
+            
+            for (int adjNode: adjList[i]) {
+                if (colors[i] == colors[adjNode]) return false;
             }
         }
-        
         return true;
         
     }
     
-    boolean dfs(int node, List<Integer>[] adjList, int[] groups, int group) {
-        groups[node] = group;
-        int antiGroup = group == 1? 2: 1;
-        
-        for (int adjNode : adjList[node]) {
-            if (groups[adjNode] != 0) {
-                if (groups[adjNode] == group) return false;
-                else continue;
-            }
-            if (!dfs(adjNode, adjList, groups, antiGroup)) return false;
-        }
-        
-        return true;
-    }
-}
-class Pair {
-    public int node;
-    public int color;
-    
-    public Pair(int a, int b) {
-        node = a;
-        color = b;
-    }
 }
